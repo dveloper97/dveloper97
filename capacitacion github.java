999ac3489da4b4723283e82bf9c85303aff9ac12
desGuatex <guatex.it.desarrollo@gmail.com>
2022-01-25 11:32:37 -0600

    Create capacitacion github.java
    [ADD]

---
+++ capacitacion github.java
@@ -0,0 +1,37 @@
+fetch origin 
+
+caminito.java
+
+agregue otra linea 
+ahora agregue otra rama llamada david
+
+****pull request
+, tomar los cambios de una rama porque lo va a mostrar en otra 
+
+master y rama 
+cuando se hace un pullrequest hace pasar todo de la rama hacia el master 
+
+solo se insertan los cambios que tenga en la rama hacia el master.
+
+base : master 
+compare: david
+
+
+comparacion de que no haya conflictos,
+
+
+otra rama prueba 
+
+
+nuvo archivo
+ejemplo.java
+
+ejemplo2.java
+[ADD] 
+
+cuando hacemos cambios realizar un pull request, solo con las opcione, luego fetch origin 
+actualizacion
+esto es una actualizacion de los dos archivos
+
+mark resolver
+commit merged
